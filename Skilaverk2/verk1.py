40347e405c1f92f56d140425c0efc15c0edf403d
Emil Newel <emilne292@tskoli.is>
2016-03-16 21:22:23 +0000

    Lul

--- Skilaverk2/verk1.py
+++ Skilaverk2/verk1.py
@@ -1,50 +1,51 @@
-import pygame
-from random import randint
+import pygame, random
+
 pygame.init()
 
-
-LEFT_BUTTON = 1
-WINDOW_SIZE = (640, 480)
-BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-x = 50
-
-running = True
-
-window = pygame.display.set_mode(WINDOW_SIZE)
-window.fill(BLACK)
+# Color constants
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BOX_COLOR = (100, 150, 100)
 
 
+window_size = window_width, window_height = 640, 480
+window = pygame.display.set_mode(window_size)
+
+# Box Color
+color = BOX_COLOR
+
+# the list to hold all the rectangles
 rect_list = []
-rect_list.append(pygame.Rect(10, 240, 100, 100))
-rect_list.append(pygame.Rect(140, 240, 100, 100))
-rect_list.append(pygame.Rect(270, 240, 100, 100))
-rect_list.append(pygame.Rect(400, 240, 100, 100))
-rect_list.append(pygame.Rect(530, 240, 100, 100))
+
+# loop control variable set to initial value of 0
+n = 0
+
+#Random number for boxes
+rnumber = random.randrange(1,6)
+
+pygame.display.set_caption('Rectangles in a Row')
+window.fill(WHITE)
+
 my_font = pygame.font.SysFont("Comic Sans MS", 30)
 
-for box in rect_list:
-    pygame.draw.rect(window, RED, box)
-    label = my_font.render(str(randint(1,6)), 1, BLUE)
-    for box in rect_list:
-        window.blit(label, (x, 260))
-        x = x =+ 130
-        pygame.display.flip()
+# rectangles with positions added to a list
+for n in range(5):
+    rect_list.append(pygame.Rect(30 + (n * 70), 30, 60, 60))
 
-
-
+# the while-loop control variable set to it's initial value
+running = True
+label = my_font.render(str(rnumber), 1, YELLOW)
+# let the games begin :-)
 while running:
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        running = False
-    elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT_BUTTON:
-        for box in rect_list:
-            if box.collidepoint(event.pos):
-                window.fill(BLACK)
-                for box in rect_list:
-                    pygame.draw.rect(window,RED, box)
-                label = my_font.render(str(randint(1,6)), 1, BLUE)
-                window.blit(label, (230, 260))
+    x = 1
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # the rectangles in the rect_list printed out
+    for r in rect_list:
+        pygame.draw.rect(window, color, r)
+        window.blit(label, (47 + (x * 70), 30))
+        x += 1
+    pygame.display.update()
 
-    pygame.display.flip()
+pygame.quit()
